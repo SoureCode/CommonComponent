921a3e876828faf7731c3b077a67caa58e2bb3f6
chapterjason <jason@sourecode.dev>
2021-05-10 23:26:09 +0200

    cut: Deprecate ResourceInterface

--- src/Model/ResourceInterface.php
+++ src/Model/ResourceInterface.php
@@ -2,6 +2,9 @@
 
 namespace SoureCode\Component\Common\Model;
 
+/**
+ * @deprecated Deprecated in 0.2.0 and will be removed in 1.0.0
+ */
 interface ResourceInterface
 {
     public function getId(): ?int;
