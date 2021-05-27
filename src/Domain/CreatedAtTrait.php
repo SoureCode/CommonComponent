1b6b4f54a72ba11285308cee6da857b36e629861
chapterjason <jason@sourecode.dev>
2021-05-27 19:30:42 +0200

    cut: Remove default orm config

--- src/Domain/CreatedAtTrait.php
+++ src/Domain/CreatedAtTrait.php
@@ -3,16 +3,9 @@
 namespace SoureCode\Component\Common\Domain;
 
 use DateTimeInterface;
-use Doctrine\ORM\Mapping as ORM;
-use Gedmo\Mapping\Annotation as Gedmo;
 
 trait CreatedAtTrait
 {
-    /**
-     * @ORM\Column(type="datetimetz")
-     *
-     * @Gedmo\Timestampable(on="create")
-     */
     protected ?DateTimeInterface $createdAt = null;
 
     public function getCreatedAt(): ?DateTimeInterface
--- src/Domain/UpdatedAtTrait.php
+++ src/Domain/UpdatedAtTrait.php
@@ -3,16 +3,9 @@
 namespace SoureCode\Component\Common\Domain;
 
 use DateTimeInterface;
-use Doctrine\ORM\Mapping as ORM;
-use Gedmo\Mapping\Annotation as Gedmo;
 
 trait UpdatedAtTrait
 {
-    /**
-     * @ORM\Column(type="datetimetz")
-     *
-     * @Gedmo\Timestampable(on="update")
-     */
     protected ?DateTimeInterface $updatedAt = null;
 
     public function getUpdatedAt(): ?DateTimeInterface
